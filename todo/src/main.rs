2a61d32ac9d44ee0984614fa5ffeb969d84eb957
Rei Okumura <19339328+kamuiroeru@users.noreply.github.com>
2021-09-11 01:52:14 +0900

    [update] P216 done

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,9 +1,20 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, http::header, post, web, App, HttpResponse, HttpServer, ResponseError};
 use askama::Template;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::params;
+use serde::Deserialize;
 use thiserror::Error;
+
+#[derive(Deserialize)]
+struct AddParams {
+    text: String,
+}
+
+#[derive(Deserialize)]
+struct DeleteParams {
+    id: u32,
+}
 
 struct TodoEntry {
     id: u32,
@@ -33,7 +44,6 @@ impl ResponseError for MyError {}
 #[get("/hello")]
 async fn hello() -> Result<HttpResponse, MyError> {
     let response_body = "Hello world!";
-    println!("request comming / !!!");
     Ok(HttpResponse::Ok().body(response_body))
 }
 
@@ -57,6 +67,32 @@ async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpRespo
     Ok(HttpResponse::Ok().content_type("text/html").body(response_body))
 }
 
+#[post("/add")]
+async fn add_todo(
+    params: web::Form<AddParams>,
+    db: web::Data<Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish()
+    )
+}
+
+#[post("/delete")]
+async fn delete_todo(
+    params: web::Form<DeleteParams>,
+    db: web::Data<Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("DELETE FROM todo WHERE id=?", &[&params.id])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish()
+    )
+}
+
 #[actix_web::main]
 async fn main() -> Result<(), actix_web::Error> {
     let manager = SqliteConnectionManager::file("todo.db");
@@ -73,7 +109,14 @@ async fn main() -> Result<(), actix_web::Error> {
     )
         .expect("Failed to create a table `todo`.");
 
-    HttpServer::new(move || App::new().service(index).data(pool.clone()))
+    HttpServer::new(move || {
+            App::new()
+                .service(hello)
+                .service(index)
+                .service(add_todo)
+                .service(delete_todo)
+                .data(pool.clone())
+        })
         .bind("0.0.0.0:8080")?
         .run()
         .await?;
